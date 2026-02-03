373c52899c0cf6115f4432677f58f220cd15cd3a
ALEX <152001452+KOR0N@users.noreply.github.com>
2026-02-03 22:03:12 +0100

    Delete src directory

--- src/firstLevel.h
+++
@@ -1,9 +0,0 @@
-#ifndef FIRSTLEVEL_H
-#define FIRSTLEVEL_H
-#include <string.h>
-#include <stdlib.h>
-
-void loadFirstLevel(char *** tab);
-
-
-#endif //FIRSTLEVEL_H
--- src/main.h
+++
@@ -1,6 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include "framework.h"
-
-#endif //MAIN_H
